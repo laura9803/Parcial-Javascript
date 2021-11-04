01e65d26dd31bbd979531f108a27f25ab08b435e
laura9803 <lazol10@hotmail.com>
2021-11-04 18:08:33 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,31 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length-1];
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next(){
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s"
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+function Prev(){
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length-1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s"
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+setInterval(function(){
+    Next();
+},4000);
+
